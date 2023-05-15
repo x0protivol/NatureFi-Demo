0f4b954e1920609942229799d5ccdb4bd7d02d4f
Luiz Felipe <68920578+luiz-k-alencar@users.noreply.github.com>
2023-05-15 02:05:47 -0300

    about page finished

--- packages/react-app/pages/about.tsx
+++ packages/react-app/pages/about.tsx
@@ -11,7 +11,98 @@ const About = () => {
       </div>
       <main className={styles.main}>
         <h1 className={styles.titlePage}>About</h1>
-        <div className={styles.newsContainer}></div>
+        <div className={styles.aboutContainer}>
+          <div className={styles.aboutRow}>
+            <div className={styles.aboutText}>
+              <h1>
+                Redeem and Retirement of carbon credits for both companies and
+                individuals
+              </h1>
+              <p>
+                Our platform offers a comprehensive solution for companies and
+                individuals who wish to offset their carbon footprint. Through
+                the Redeem and Retirement feature, we facilitate the process of
+                purchasing and redeeming carbon credits. Companies can acquire
+                credits to compensate for their emissions, while individuals
+                also have the opportunity to contribute to a more sustainable
+                future. By being part of our community, you will have access to
+                a wide range of trustworthy carbon offset projects and
+                initiatives, allowing you to make a difference in combating
+                climate change.
+              </p>
+            </div>
+            <div className={styles.aboutImage}>
+              <img src="/nftsImages2.svg" alt="nftImages" />
+            </div>
+          </div>
+          <div className={styles.aboutRow}>
+            <div className={styles.aboutImage}>
+              <img src="/web3auth_modal.png" alt="web3auth_modal" />
+            </div>
+            <div className={styles.aboutText}>
+              <h1>
+                Democratization of access to web3 technology through social
+                media login and automatic wallet creation
+              </h1>
+              <p>
+                We aim to make web3 technology accessible to all users,
+                regardless of their previous experience. With the goal of
+                simplifying the onboarding process to our platform, we offer the
+                option of social media login, allowing you to easily connect and
+                start exploring the features immediately. Additionally, we
+                provide automatic wallet creation, eliminating the need for
+                advanced technical knowledge to set up a crypto wallet. By
+                removing these entry barriers, we are empowering more people to
+                participate in the carbon tokenization economy and contribute to
+                a sustainable future.
+              </p>
+            </div>
+          </div>
+          <div className={styles.aboutRow}>
+            <div className={styles.aboutText}>
+              <h1>
+                Calculation of your carbon footprint with artificial
+                intelligence
+              </h1>
+              <p>
+                We understand that awareness is the first step towards change.
+                That{"'"}s why our platform offers an advanced carbon footprint
+                calculation functionality. Using artificial intelligence, we
+                collect relevant information about your lifestyle, consumption
+                habits, and daily activities to provide an accurate estimate of
+                your carbon footprint. With this data, you will gain a clear
+                understanding of the environmental impact of your actions and
+                make informed decisions to reduce your contribution to carbon
+                emissions.
+              </p>
+            </div>
+            <div className={styles.aboutImage}>
+              <img
+                className={styles.aiModal}
+                src="/aiModal.png"
+                alt="aiModal"
+              />
+            </div>
+          </div>
+          <div className={styles.aboutRow}>
+            <div className={styles.aboutImage}>
+              <img className={styles.aiModal} src="/news.png" alt="news_image" />
+            </div>
+            <div className={styles.aboutText}>
+              <h1>News section about the world of carbon credits</h1>
+              <p>
+                In our platform, we believe in the importance of education and
+                knowledge sharing. Therefore, we offer a dedicated news section
+                with updated information about the world of carbon credits. In
+                this section, you will find articles, analyses, and relevant
+                insights about the latest trends, regulations, and innovations
+                in this ever-evolving field. Stay informed about the latest
+                developments and be inspired by success stories of
+                sustainability projects worldwide.
+              </p>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
